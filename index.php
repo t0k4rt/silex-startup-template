f732021a8d67b408ee7a02f41f27535f57bc97fb
Alexandre Assouad <alexandre.assouad@gmail.com>
2015-05-22 13:35:29 +0200

    Trying to send monolog logs to stderr / stdout

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 require_once __DIR__.'/vendor/autoload.php'; 
 
 $app = new Silex\Application(); 
+$app['debug'] = true;
 
 $app->register(new Predis\Silex\ClientServiceProvider(), [
     'predis.parameters' => 'tcp://redis:6379',
@@ -11,7 +12,12 @@ $app->register(new Predis\Silex\ClientServiceProvider(), [
     ],
 ]);
 
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+    'monolog.logfile' => 'php://stdout',
+));
+
 $app->get('/', function() use($app) { 
+    $app['monolog']->addInfo("log test");
     return 'Hello World'; 
 }); 
 
